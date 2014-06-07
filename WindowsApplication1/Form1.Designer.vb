3850d5741c0cfe80b7aa2aa0b9b6e1765c10cfa6
mahan2 <mahan2@pdx.edu>
2014-06-07 13:55:10 -0700

    Wokring

---
+++ WindowsApplication1/Form1.Designer.vb
@@ -0,0 +1,280 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.ContextMenuStrip1 = New System.Windows.Forms.ContextMenuStrip(Me.components)
+        Me.ToolStripMenuItem1 = New System.Windows.Forms.ToolStripMenuItem()
+        Me.SettingsToolStripMenuItem = New System.Windows.Forms.ToolStripMenuItem()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.NumericUpDown1 = New System.Windows.Forms.NumericUpDown()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.userIPrange0 = New System.Windows.Forms.TextBox()
+        Me.userIPrange1 = New System.Windows.Forms.TextBox()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.Label6 = New System.Windows.Forms.Label()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.Install = New System.Windows.Forms.Button()
+        Me.InstallLocation = New System.Windows.Forms.FolderBrowserDialog()
+        Me.ContextMenuStrip1.SuspendLayout()
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ContextMenuStrip1
+        '
+        Me.ContextMenuStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.ToolStripMenuItem1, Me.SettingsToolStripMenuItem})
+        Me.ContextMenuStrip1.Name = "ContextMenuStrip1"
+        Me.ContextMenuStrip1.Size = New System.Drawing.Size(172, 48)
+        '
+        'ToolStripMenuItem1
+        '
+        Me.ToolStripMenuItem1.Name = "ToolStripMenuItem1"
+        Me.ToolStripMenuItem1.Size = New System.Drawing.Size(171, 22)
+        Me.ToolStripMenuItem1.Text = "Close DMCA saver"
+        '
+        'SettingsToolStripMenuItem
+        '
+        Me.SettingsToolStripMenuItem.Name = "SettingsToolStripMenuItem"
+        Me.SettingsToolStripMenuItem.Size = New System.Drawing.Size(171, 22)
+        Me.SettingsToolStripMenuItem.Text = "Settings"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(15, 216)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(75, 23)
+        Me.Button1.TabIndex = 1
+        Me.Button1.Text = "Hide"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'TextBox1
+        '
+        Me.TextBox1.Location = New System.Drawing.Point(159, 22)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(100, 20)
+        Me.TextBox1.TabIndex = 2
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 25)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(141, 13)
+        Me.Label1.TabIndex = 3
+        Me.Label1.Text = "Name of file sharing program"
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(159, 111)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(40, 23)
+        Me.Button2.TabIndex = 4
+        Me.Button2.Text = "Save"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(205, 111)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(54, 23)
+        Me.Button3.TabIndex = 5
+        Me.Button3.Text = "Test"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(96, 216)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(75, 23)
+        Me.Button4.TabIndex = 6
+        Me.Button4.Text = "Start"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(177, 221)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(96, 13)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Status: not running"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 74)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(0, 13)
+        Me.Label3.TabIndex = 8
+        '
+        'NumericUpDown1
+        '
+        Me.NumericUpDown1.Increment = New Decimal(New Integer() {30, 0, 0, 0})
+        Me.NumericUpDown1.Location = New System.Drawing.Point(213, 48)
+        Me.NumericUpDown1.Maximum = New Decimal(New Integer() {300, 0, 0, 0})
+        Me.NumericUpDown1.Minimum = New Decimal(New Integer() {30, 0, 0, 0})
+        Me.NumericUpDown1.Name = "NumericUpDown1"
+        Me.NumericUpDown1.Size = New System.Drawing.Size(46, 20)
+        Me.NumericUpDown1.TabIndex = 9
+        Me.NumericUpDown1.Value = New Decimal(New Integer() {30, 0, 0, 0})
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 50)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(190, 13)
+        Me.Label4.TabIndex = 10
+        Me.Label4.Text = "Check IP address frequency (seconds)"
+        '
+        'userIPrange0
+        '
+        Me.userIPrange0.Location = New System.Drawing.Point(159, 77)
+        Me.userIPrange0.MaxLength = 3
+        Me.userIPrange0.Name = "userIPrange0"
+        Me.userIPrange0.Size = New System.Drawing.Size(30, 20)
+        Me.userIPrange0.TabIndex = 11
+        '
+        'userIPrange1
+        '
+        Me.userIPrange1.Location = New System.Drawing.Point(205, 77)
+        Me.userIPrange1.MaxLength = 3
+        Me.userIPrange1.Name = "userIPrange1"
+        Me.userIPrange1.Size = New System.Drawing.Size(31, 20)
+        Me.userIPrange1.TabIndex = 12
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Font = New System.Drawing.Font("Microsoft Sans Serif", 12.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label5.Location = New System.Drawing.Point(190, 76)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(93, 20)
+        Me.Label5.TabIndex = 13
+        Me.Label5.Text = ".        . * . *"
+        '
+        'Label6
+        '
+        Me.Label6.AutoSize = True
+        Me.Label6.Location = New System.Drawing.Point(12, 81)
+        Me.Label6.Name = "Label6"
+        Me.Label6.Size = New System.Drawing.Size(128, 13)
+        Me.Label6.TabIndex = 14
+        Me.Label6.Text = "Campus IP address range"
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(15, 111)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(75, 23)
+        Me.Button5.TabIndex = 15
+        Me.Button5.Text = "Get IP range"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.BalloonTipText = "Campus "
+        Me.NotifyIcon1.ContextMenuStrip = Me.ContextMenuStrip1
+        Me.NotifyIcon1.Icon = CType(resources.GetObject("NotifyIcon1.Icon"), System.Drawing.Icon)
+        Me.NotifyIcon1.Text = "DMCA SAVER"
+        Me.NotifyIcon1.Visible = True
+        '
+        'Install
+        '
+        Me.Install.Location = New System.Drawing.Point(205, 148)
+        Me.Install.Name = "Install"
+        Me.Install.Size = New System.Drawing.Size(53, 23)
+        Me.Install.TabIndex = 16
+        Me.Install.Text = "Install"
+        Me.Install.UseVisualStyleBackColor = True
+        '
+        'InstallLocation
+        '
+        Me.InstallLocation.Description = "Please select a location to install. A folder will be created under the location " & _
+    "you select."
+        Me.InstallLocation.RootFolder = System.Environment.SpecialFolder.MyComputer
+        Me.InstallLocation.SelectedPath = "C:\Program Files (x86)"
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(284, 262)
+        Me.ControlBox = False
+        Me.Controls.Add(Me.Install)
+        Me.Controls.Add(Me.Button5)
+        Me.Controls.Add(Me.Label6)
+        Me.Controls.Add(Me.userIPrange1)
+        Me.Controls.Add(Me.userIPrange0)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.NumericUpDown1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Button4)
+        Me.Controls.Add(Me.Button3)
+        Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.Label5)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle
+        Me.Name = "Form1"
+        Me.Text = "Campus DMCA preventer"
+        Me.ContextMenuStrip1.ResumeLayout(False)
+        CType(Me.NumericUpDown1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents ContextMenuStrip1 As System.Windows.Forms.ContextMenuStrip
+    Friend WithEvents ToolStripMenuItem1 As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents SettingsToolStripMenuItem As System.Windows.Forms.ToolStripMenuItem
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents TextBox1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label1 As System.Windows.Forms.Label
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents Label2 As System.Windows.Forms.Label
+    Friend WithEvents Label3 As System.Windows.Forms.Label
+    Friend WithEvents NumericUpDown1 As System.Windows.Forms.NumericUpDown
+    Friend WithEvents Label4 As System.Windows.Forms.Label
+    Friend WithEvents userIPrange0 As System.Windows.Forms.TextBox
+    Friend WithEvents userIPrange1 As System.Windows.Forms.TextBox
+    Friend WithEvents Label5 As System.Windows.Forms.Label
+    Friend WithEvents Label6 As System.Windows.Forms.Label
+    Friend WithEvents Button5 As System.Windows.Forms.Button
+    Friend WithEvents NotifyIcon1 As System.Windows.Forms.NotifyIcon
+    Friend WithEvents Install As System.Windows.Forms.Button
+    Friend WithEvents InstallLocation As System.Windows.Forms.FolderBrowserDialog
+
+End Class
---
+++ WindowsApplication1/Form1.vb
@@ -0,0 +1,213 @@
+﻿Imports System.Web.HttpContext
+Imports System.Net
+Imports System.IO
+Imports System.Math
+
+
+Public Class Form1
+    Dim myThread As System.Threading.Thread
+    Dim firstRun As Boolean = False
+    Dim programName As String
+    Dim wasKilled As Boolean = False
+    Dim timeout As Integer
+    Dim timeoutString As String
+    Dim ipRange() As String
+
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        CheckForFirstRunUpdateAndStartThread()
+    End Sub
+    Private Sub CheckForFirstRunUpdateAndStartThread()
+        GetSettings()
+
+        If programName = "" Then
+            MsgBox("Disclaimer: This program is designed for PSU main campus WiFi and VPN. This program will not work on many of the dorm's internet. The author assumes no liability from DMCA violations recieved while using this program. This program is to be used as a backup to manually checking if your file sharing program is running before connecting to campus internet/vpn.")
+            MsgBox("Please specify the name of your torrent program")
+            firstRun = True
+        Else
+            UpdateUserControls()
+            SetTimeout()
+            StartCheckerThread()
+        End If
+    End Sub
+    Private Sub UpdateUserControls()
+        TextBox1.Text = programName
+        NumericUpDown1.Value = SetValue(timeoutString)
+        userIPrange0.Text = ipRange(0)
+        userIPrange1.Text = ipRange(1)
+
+    End Sub
+    Private Sub SetTimeout()
+        timeout = NumericUpDown1.Value * 1000
+    End Sub
+    Public Shared Function SetValue(timeoutString As String) As Integer
+        Return Convert.ToInt32(timeoutString)
+    End Function
+
+    Private Sub StartCheckerThread()
+        myThread = New System.Threading.Thread(AddressOf IpAddressChecker)
+        myThread.Start()
+        Label2.Text = "Status: running"
+        Button4.Enabled = False
+    End Sub
+    Private Sub StopCheckerThread()
+        If Not firstRun Then
+            myThread.Abort()
+            Label2.Text = "Status: not running"
+            Button4.Enabled = True
+            Refresh()
+        End If
+    End Sub
+    Private Function GetIPAddress() As String
+        Dim wc As New WebClient
+        Dim ipEXT As String
+        Try
+            ipEXT = wc.DownloadString("http://icanhazip.com")
+            Exit Try
+        Catch ex As Exception
+            'MsgBox(ex.Message.ToString)
+            'wc.Dispose()
+            Return "0.0.0.0"
+        End Try
+        'wc.Dispose()
+        Return ipEXT
+    End Function
+    Private Sub IpAddressChecker()
+        'Dim ipAddress As String
+        Dim ipArray As String()
+
+        While True
+            Threading.Thread.Sleep(timeout)
+            'ipAddress =
+            ipArray = GetIPAddress().Split(".")
+            ' MsgBox(ipRange(0) + "." + ipRange(1))
+            If ipArray(0) = ipRange(0) And ipArray(1) = ipRange(1) Then
+                KillFileSharing()
+            End If
+            GC.Collect()
+        End While
+    End Sub
+
+    Private Sub ToolStripMenuItem1_Click(sender As Object, e As EventArgs) Handles ToolStripMenuItem1.Click
+        myThread.Abort()
+        Close()
+    End Sub
+
+    Private Sub SettingsToolStripMenuItem_Click(sender As Object, e As EventArgs) Handles SettingsToolStripMenuItem.Click
+        ShowSettings()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        HideForm()
+    End Sub
+
+    Private Sub ShowSettings()
+        Show()
+        NotifyIcon1.Visible = False
+    End Sub
+    Private Sub HideForm()
+        Hide()
+        NotifyIcon1.Visible = True
+    End Sub
+    Private Sub KillFileSharing()
+        Dim pProcess() As Process = System.Diagnostics.Process.GetProcessesByName(programName)
+        For Each p As Process In pProcess
+            p.Kill()
+            MsgBox("You've been saved from a potential DMCA violation running " + programName.ToUpper + " on campus ip address space", vbSystemModal + vbExclamation)
+            wasKilled = True
+        Next
+    End Sub
+
+    Private Sub NotifyIcon1_MouseDoubleClick(sender As Object, e As MouseEventArgs) Handles NotifyIcon1.MouseDoubleClick
+        ShowSettings()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        Dim txtinput As String()
+        txtinput = TextBox1.Text.Split(".")
+
+        If txtinput.Length > 1 Then
+            MsgBox("Please enter only the process name, not the .exe portion. Please check correction and resave.")
+            TextBox1.Text = txtinput(0)
+        Else
+
+            SaveAndUpdateSettings()
+
+            MsgBox("Setting saved, please launch your program and use the test button")
+            StopCheckerThread()
+        End If
+    End Sub
+    Private Sub GetSettings()
+        timeoutString = GetSetting("TestApp", "settings", "timeout")
+        programName = GetSetting("TestApp", "settings", "program name")
+        ' Dim ipRangeTemp As String =
+        ipRange = GetSetting("TestApp", "settings", "ipPrefix").Split(".")
+    End Sub
+    Private Sub SaveAndUpdateSettings()
+        SaveSetting("TestApp", "settings", "program name", TextBox1.Text)
+        SaveSetting("TestApp", "settings", "timeout", NumericUpDown1.Value.ToString)
+        SaveSetting("TestApp", "settings", "ipPrefix", userIPrange0.Text + "." + userIPrange1.Text)
+        ipRange = GetSetting("TestApp", "settings", "ipPrefix").Split(".")
+        programName = TextBox1.Text
+        SetTimeout()
+    End Sub
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        wasKilled = False
+        Dim wc As New WebClient
+        Dim ipArray = GetIPAddress().Split(".")
+
+        KillFileSharing()
+        If wasKilled = False Then
+            MsgBox("Please verify the process name of your program, it may not be running. EX: utorrent")
+        End If
+        If ipArray(0) = ipRange(0) And ipArray(1) = ipRange(1) Then
+            MsgBox("You are on campus IP address space and can recieve a DMCA with any file sharing")
+        Else
+            MsgBox("According to the settings, you are not on campus internet. Your IP address is: " + String.Join(".", ipArray))
+        End If
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        StartCheckerThread()
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        Dim temp As Integer = MsgBox("Are you on campus internet/vpn?", MsgBoxStyle.YesNo)
+        If temp = 6 Then
+            Dim ipArray() As String = GetIPAddress().Split(".")
+            If ipArray(0) = 0 And ipArray(1) = 0 Then
+                MsgBox("Internet error, not connected, please verify connection")
+            Else
+                userIPrange0.Text = ipArray(0)
+                userIPrange1.Text = ipArray(1)
+                MsgBox("Please save your settings if the information is correct")
+            End If
+
+        Else
+            MsgBox("Please connect to campus internet and try again.")
+        End If
+        
+    End Sub
+
+
+    Private Sub Install_Click(sender As Object, e As EventArgs) Handles Install.Click
+        Dim exePathAndName As String = System.Diagnostics.Process.GetCurrentProcess().MainModule.FileName
+        Dim appData As String = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData)
+        Dim installPath As String = appData + "\DMCA Preventer"
+        Dim filePathAndName As String = installPath + "\DMCA.exe"
+        Dim curUserRegRun As String = "HKEY_CURRENT_USER\Software\Microsoft\Windows\CurrentVersion\Run"
+        Dim RegKeyName As String = "DMCA Preventer"
+
+        Try
+            My.Computer.FileSystem.CreateDirectory(installPath)
+            My.Computer.FileSystem.CopyFile(exePathAndName, filePathAndName, overwrite:=True)
+            My.Computer.Registry.SetValue(curUserRegRun, RegKeyName, filePathAndName)
+            MsgBox("Install Sucessful")
+        Catch ex As Exception
+            'Debuging for now, proper exception handling needed
+            MsgBox(ex.ToString)
+        End Try
+
+    End Sub
+End Class
+
